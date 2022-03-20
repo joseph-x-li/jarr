64001f57cd2ca12e73d4163f211ca97484f0e792
Joseph Li <13826851+joseph-x-li@users.noreply.github.com>
2022-03-20 14:17:56 -0400

    asdf

---
+++ client.py
@@ -0,0 +1,70 @@
+import tkinter as tk
+import zmq
+
+# send self.hold to server using push/pull zmq sockets
+
+class Example(tk.Frame):
+    def __init__(self, parent):
+        tk.Frame.__init__(self, parent, width=400,  height=400)
+
+        self.label = tk.Label(self, text="last key pressed:  ", width=100)
+        self.label.pack(fill="both", padx=100, pady=100)
+
+        self.label.bind("<w>", self.on_wasd)
+        self.label.bind("<KeyRelease-w>", self.on_wasd_release)
+        self.label.bind("<a>", self.on_wasd)
+        self.label.bind("<KeyRelease-a>", self.on_wasd_release)
+        self.label.bind("<s>", self.on_wasd)
+        self.label.bind("<KeyRelease-s>", self.on_wasd_release)
+        self.label.bind("<d>", self.on_wasd)
+        self.label.bind("<KeyRelease-d>", self.on_wasd_release)
+        # up arrow
+        self.label.bind("<Up>", self.on_wasd)
+        self.label.bind("<KeyRelease-Up>", self.on_wasd_release)
+        # down arrow
+        self.label.bind("<Down>", self.on_wasd)
+        self.label.bind("<KeyRelease-Down>", self.on_wasd_release)
+        # left arrow
+        self.label.bind("<Left>", self.on_wasd)
+        self.label.bind("<KeyRelease-Left>", self.on_wasd_release)
+        # right arrow
+        self.label.bind("<Right>", self.on_wasd)
+        self.label.bind("<KeyRelease-Right>", self.on_wasd_release)
+
+        # give keyboard focus to the label by default, and whenever
+        # the user clicks on it
+        self.label.focus_set()
+        self.label.bind("<1>", lambda event: self.label.focus_set())
+
+
+        # my shit
+        self.hold = {
+          "w": False,
+          "a": False,
+          "s": False,
+          "d": False,
+          "Up": False,
+          "Down": False,
+          "Left": False,
+          "Right": False
+        }
+
+        self.context = zmq.Context()
+        self.socket = self.context.socket(zmq.PUSH)
+        self.socket.connect("tcp://192.168.4.3:5555")
+
+    def on_wasd(self, event):
+        self.hold[event.keysym] = True
+        self.label.configure(text=f"hold: {self.hold} last key pressed: {event.keysym}")
+        self.socket.send_pyobj(self.hold, flags=zmq.NOBLOCK)
+
+    def on_wasd_release(self, event):
+        self.hold[event.keysym] = False
+        self.label.configure(text=f"hold: {self.hold} last key released: {event.keysym}")
+        self.socket.send_pyobj(self.hold, flags=zmq.NOBLOCK)
+
+
+if __name__ == "__main__":
+    root = tk.Tk()
+    Example(root).pack(fill="both", expand=True)
+    root.mainloop()
---
+++ server.py
@@ -0,0 +1,111 @@
+from tkinter import LEFT
+import RPi.GPIO as GPIO
+import time
+import zmq
+
+
+PAN_PIN = 32
+TILT_PIN = 36
+STEERING_PIN = 40
+
+in1 = 31
+in2 = 33
+in3 = 35
+in4 = 37
+
+
+GPIO.setmode(GPIO.BOARD)
+GPIO.setup(PAN_PIN, GPIO.OUT)
+GPIO.setup(TILT_PIN, GPIO.OUT)
+GPIO.setup(STEERING_PIN, GPIO.OUT)
+GPIO.setup(in1, GPIO.OUT)
+GPIO.setup(in2, GPIO.OUT)
+GPIO.setup(in3, GPIO.OUT)
+GPIO.setup(in4, GPIO.OUT)
+
+steering = GPIO.PWM(STEERING_PIN, 50)
+steering.start(0)
+
+pan = GPIO.PWM(PAN_PIN, 50)
+pan.start(0)
+
+tilt = GPIO.PWM(TILT_PIN, 50)
+tilt.start(0)
+
+# listen on TCP port 5555 for incoming zmq PUSH messages
+
+context = zmq.Context()
+socket = context.socket(zmq.PULL)
+socket.bind("tcp://*:5555")
+
+try:
+  while True:
+    #Ask user for angle and turn servo to it
+    try:
+      keyboard_state = socket.recv_pyobj()
+      # handle wasd for forward and back
+      if keyboard_state["w"]:
+        GPIO.output(in1, True)
+        GPIO.output(in2, False)
+        GPIO.output(in3, True)
+        GPIO.output(in4, False)
+      elif keyboard_state["s"]:
+        GPIO.output(in1, False)
+        GPIO.output(in2, True)
+        GPIO.output(in3, False)
+        GPIO.output(in4, True)
+      else:
+        GPIO.output(in1, False)
+        GPIO.output(in2, False)
+        GPIO.output(in3, False)
+        GPIO.output(in4, False)
+
+      # handle wasd for left and right
+      if keyboard_state["a"]:
+        STEERING_LEFT_ANGLE = 60
+        steering.ChangeDutyCycle(2+(STEERING_LEFT_ANGLE/18))
+      elif keyboard_state["d"]:
+        STEERING_RIGHT_ANGLE = 120
+        steering.ChangeDutyCycle(2+(STEERING_RIGHT_ANGLE/18))
+      else:
+        STEERING_CENTER_ANGLE = 90
+        steering.ChangeDutyCycle(2+(STEERING_CENTER_ANGLE/18))
+
+      # handle pan by checking "Left" and "Right" keys
+      if keyboard_state["Left"]:
+        PAN_LEFT_ANGLE = 45
+        pan.ChangeDutyCycle(2+(PAN_LEFT_ANGLE/18))
+      elif keyboard_state["Right"]:
+        PAN_RIGHT_ANGLE = 135
+        pan.ChangeDutyCycle(2+(PAN_RIGHT_ANGLE/18))
+      else:
+        PAN_CENTER_ANGLE = 90
+        pan.ChangeDutyCycle(2+(PAN_CENTER_ANGLE/18))
+
+      # handle tilt by checking "Up" and "Down" keys
+      if keyboard_state["Up"]:
+        TILT_UP_ANGLE = 45
+        tilt.ChangeDutyCycle(2+(TILT_UP_ANGLE/18))
+      elif keyboard_state["Down"]:
+        TILT_DOWN_ANGLE = 135
+        tilt.ChangeDutyCycle(2+(TILT_DOWN_ANGLE/18))
+      else:
+        TILT_CENTER_ANGLE = 90
+        tilt.ChangeDutyCycle(2+(TILT_CENTER_ANGLE/18))
+
+    except zmq.ZMQError:
+      print("WTF???")
+      continue
+
+finally:
+  #Clean things up at the end
+  steering.stop()
+  pan.stop()
+  tilt.stop()
+  GPIO.output(in1, False)
+  GPIO.output(in2, False)
+  GPIO.output(in3, False)
+  GPIO.output(in4, False)
+  GPIO.cleanup()
+  print("Goodbye!")
+
